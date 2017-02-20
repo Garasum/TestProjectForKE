9cb11b5b7b201ce92513fdbb9cffa02b796aae1e
Orest <Orestgar@gmail.com>
2017-02-20 22:23:06 +0200

    Trying AbstractFactory - Done

--- patterntest/src/main/java/creational/AbstractFactory.java
+++ patterntest/src/main/java/creational/AbstractFactory.java
@@ -5,11 +5,32 @@ package creational;
  */
 public class AbstractFactory {
 
-
+    //Abstract Factory
     public interface IToyFactory {
         Bear GetBear();
-
         Cat GetCat();
+    }
+
+    //Concrete Factory
+    public class TeddyToysFactory implements IToyFactory{
+        public Bear GetBear() {
+            return new TeddyBear();
+        }
+
+        public Cat GetCat() {
+            return new TeddyCat();
+        }
+    }
+
+    //Concrete Factory
+    public class WoodenToysFactory implements IToyFactory{
+        public Bear GetBear() {
+            return new WoodenBear();
+        }
+
+        public Cat GetCat() {
+            return new WoodenCat();
+        }
     }
 
     abstract class AnimalToy {
@@ -30,6 +51,35 @@ public class AbstractFactory {
 
     abstract class Cat extends AnimalToy {
         protected String name;
+    }
+
+    class WoodenCat extends Cat{
+        public WoodenCat() {
+        }
+    }
+
+    class TeddyCat extends Cat{
+        public TeddyCat() {
+        }
+    }
+
+    class WoodenBear extends Bear{
+        public WoodenBear() {
+        }
+    }
+
+    class TeddyBear extends Bear{
+        public TeddyBear() {
+        }
+    }
+
+
+    public static void main(String[] args) {
+        AbstractFactory abstractFactory = new AbstractFactory();
+        IToyFactory toyFactory = abstractFactory.new WoodenToysFactory();
+        Bear bear = toyFactory.GetBear();
+        Cat cat = toyFactory.GetCat();
+        System.out.println(bear.getName() + cat.getName());
     }
 
 }
---
+++ patterntest/src/main/java/creational/Builder.java
@@ -0,0 +1,7 @@
+package creational;
+
+/**
+ * Created by Orest on 2/20/2017.
+ */
+public class Builder {
+}

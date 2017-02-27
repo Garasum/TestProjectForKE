33f949251b9ae339159cdb9399192d655cecaf46
oharas <h55-ud3h>
2017-02-27 18:40:03 +0200

    Prototype pattern - Work In Progress

--- patterntest/src/main/java/creational/Prototype.java
+++ patterntest/src/main/java/creational/Prototype.java
@@ -3,5 +3,60 @@ package creational;
 /**
  * Created by Orest on 2/23/2017.
  */
-public class Prototype   {
+public class Prototype {
+
+    public class CalendarPrototype {
+        public CalendarPrototype CloneObject() throws CloneNotSupportedException {
+            Object copyOfPrototype = this.clone();
+            return (CalendarPrototype) copyOfPrototype;
+        }
+    }
+
+    public class CalendarEvent extends CalendarPrototype {
+        public Attendee[] attendees;
+        public Priority priority;
+        //public DateTime startDateAndTime;
+
+        public CalendarPrototype CloneObject(){
+            // Continue Here 
+            return null;
+        }
+    }
+
+
+    public class Attendee {
+        public String firstName;
+        public String lastName;
+        public String emailAdress;
+    }
+
+    public class Priority {
+        private Integer priority;
+
+        private Priority(Integer priority) {
+            this.priority = priority;
+        }
+
+        public Object CloneObject() throws CloneNotSupportedException {
+            return this.clone();
+        }
+
+        public Priority High() {
+            return new Priority(1);
+        }
+
+        public Priority Low() {
+            return new Priority(-1);
+        }
+
+        public void SetPriorityValue(Integer priority) {
+            this.priority = priority;
+        }
+
+        public boolean IsHigh() {
+            return priority > 0;
+        }
+    }
+
+
 }

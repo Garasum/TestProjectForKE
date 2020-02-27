560f55e6b04fa1df572cbfd4764ce95138d577a6
Orest <Orestgar@gmail.com>
2020-02-28 01:02:09 +0200

    how to create new object and pass it to main

--- patterntest/src/main/java/temp/ditask/DITest.java
+++ patterntest/src/main/java/temp/ditask/DITest.java
@@ -1,13 +1,13 @@
 package temp.ditask;
 
-import static temp.ditask.Ref.init;
+import static temp.ditask.Reff.init;
 
 public class DITest {
 
     @Auto(name = "Apple")
     IApple apple;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
         init();
         //apple.hello();
     }

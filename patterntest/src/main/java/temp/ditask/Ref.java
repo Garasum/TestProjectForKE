7cef06da0549b322b6b3bc4d43415d78bc3bd608
Orest Harasym <ext-orest.harasym@here.com>
2020-03-11 18:37:01 +0200

    work in progress - used Reflections library

--- patterntest/src/main/java/temp/ditask/Ref.java
+++ patterntest/src/main/java/temp/ditask/Ref.java
@@ -1,12 +1,21 @@
 package temp.ditask;
 
 
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 import org.testng.internal.reflect.ReflectionHelper;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 public class Ref {
 
@@ -14,6 +23,7 @@ public class Ref {
         //Find classes that use annotation - Scan Package
         //Find classes that implement Interface IApple - Scan Package
         Class<DITest> obj = DITest.class;
+        System.out.println(obj.getPackage());
 
         for (Field field : obj.getDeclaredFields())
             if (field.isAnnotationPresent(Auto.class)) {
@@ -23,19 +33,6 @@ public class Ref {
 
                 System.out.println("Annotation name value : " + auto);
                 System.out.println("Annotation class value : " + auto.getClass());
-
-                try {
-                    // use
-                    String className = "temp.ditask.Apple";
-                    System.out.println(className);
-                    Class<?> cls = Class.forName(className);
-                    IApple apple = (IApple) cls.newInstance();
-
-                    apple.hello();
-
-                } catch (Throwable ex) {
-                    System.out.println(ex);
-                }
             }
     }
 
@@ -56,18 +53,26 @@ public class Ref {
         Package pack = IApple.class.getPackage();
         System.out.println(pack);
 
+        Reflections reflections = new Reflections(pack);
+        Set<Class<? extends IApple>> classes = reflections.getSubTypesOf(IApple.class);
+        //https://www.edureka.co/community/11339/retrieve-all-the-implementations-of-an-interface-in-java
+        for (Object o : classes){
+            System.out.println(o);
+        }
+
+
         //https://stackoverflow.com/questions/520328/can-you-find-all-classes-in-a-package-using-reflection
         //http://oohhyeah.blogspot.com/2012/01/use-java-reflection-to-find-classes.html
-        
+
         //scan package for classes
-            // chack in class - Class<?> cls = searchClass.getInterfaces();
-            // if cls implements IApple - return it
+        // chack in class - Class<?> cls = searchClass.getInterfaces();
+        // if cls implements IApple - return it
 
     }
 
 
     public static void main(String[] args) throws ClassNotFoundException {
-        //init();
+        init();
         String className = "temp.ditask.Apple";
         Class<?> cls = Class.forName(className);
 
